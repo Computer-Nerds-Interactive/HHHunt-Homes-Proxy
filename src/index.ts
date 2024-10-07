aa79bde1da1c1f4b31a3fc726b47f730f083bdf2
MHinson0348 <madison@cni-inc.com>
2024-10-07 14:56:12 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ const app = new Hono()
 
 const webflowDomain = "https://hhhunthomesdev.com"
 
+
 // Directly handle specific URLs you want to ignore
 app.get("/new-homes/virginia/richmond/communities", async (c) => {
   return fetch(c.req.raw) // Simply fetch the URL as is, or return a default response
@@ -25,7 +26,9 @@ app.get("/new-homes/north-carolina/southern-pines-carthage/communities", async (
   return fetch(c.req.raw)
 })
 
-// Define other routes
+// ***** Define other routes
+
+//communities
 app.get("/new-homes/:state/:city/:id", async (c) => {
   const { state, city, id } = c.req.param()
   const url = new URL(
@@ -38,6 +41,7 @@ app.get("/new-homes/:state/:city/:id", async (c) => {
   return fetch(url)
 })
 
+//floor plans
 app.get("/new-homes/:state/:city/:community/floorplans/:slug", async (c) => {
   const { state, city, community, slug } = c.req.param()
   const url = new URL(
@@ -50,7 +54,22 @@ app.get("/new-homes/:state/:city/:community/floorplans/:slug", async (c) => {
   return fetch(url)
 })
 
-// Handle other cases or redirects as needed
+//move-in-ready homes
+app.get("/new-homes/:state/:city/:community/move-in-ready-homes/:address", async (c) => {
+  const { state, city, community, address } = c.req.param()
+  const url = new URL(
+    `${webflowDomain}/new-homes/move-in-ready-homes/${address}--${community}--${city}--${state}`
+  )
+  const originalUrl = new URL(c.req.url)
+  for (const [key, value] of originalUrl.searchParams) {
+    url.searchParams.set(key, value)
+  }
+  return fetch(url)
+})
+
+// ***** Handle other cases or redirects as needed
+
+/*
 app.get("/new-homes/region/:slug", async (c) => {
   const { slug } = c.req.param()
   const [city, state] = slug.split("--")
@@ -61,6 +80,8 @@ app.get("/new-homes/region/:slug", async (c) => {
   }
   return c.redirect(url.toString())
 })
+
+*/
 
 app.get("/new-homes/communities/:slug", async (c) => {
   const { slug } = c.req.param()
@@ -80,6 +101,19 @@ app.get("/new-homes/floor-plans-and-models/:slug", async (c) => {
   const [floorPlanName, community, state, city] = slug.split("--")
   const url = new URL(
     `${webflowDomain}/new-homes/${state}/${city}/${community}/floorplans/${floorPlanName}`
+  )
+  const originalUrl = new URL(c.req.url)
+  for (const [key, value] of originalUrl.searchParams) {
+    url.searchParams.set(key, value)
+  }
+  return c.redirect(url.toString())
+})
+
+app.get("/new-homes/move-in-ready-homes/:slug", async (c) => {
+  const { slug } = c.req.param()
+  const [specAddress, community, state, city] = slug.split("--")
+  const url = new URL(
+    `${webflowDomain}/new-homes/${state}/${city}/${community}/move-in-ready-homes/${specAddress}`
   )
   const originalUrl = new URL(c.req.url)
   for (const [key, value] of originalUrl.searchParams) {
