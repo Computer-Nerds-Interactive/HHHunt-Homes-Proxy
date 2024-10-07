233765d068d5fec6c44452760be7624364e949f9
MHinson0348 <madison@cni-inc.com>
2024-10-07 15:13:15 -0400

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -5,22 +5,13 @@ const app = new Hono()
 const webflowDomain = "https://hhhunthomesdev.com"
 
 
-/*
-app.get("/new-homes/:state/:city/communities", async (c) => {
-  const { city, state } = c.req.param()
-  console.log(state, city)
 
-  //Get the communities from the webflow
-  const url = new URL(`${webflowDomain}/new-homes/region/${city}--${state}`)
-  console.log(url.toString())
-  const originalUrl = new URL(c.req.url)
-  for (const [key, value] of originalUrl.searchParams) {
-    url.searchParams.set(key, value)
-  }
+app.get("/new-homes/virginia/richmond/communities", async (c) => {
+  const url = new URL(`${webflowDomain}/new-homes/virginia/richmond/communities`)
   return fetch(url)
 })
 
-*/
+
 
 app.get("/new-homes/:state/:city/:id", async (c) => {
   const { state, city, id } = c.req.param()
@@ -52,22 +43,6 @@ app.get("/new-homes/:state/:city/:community/floorplans/:slug", async (c) => {
 })
 
 //Redirect to the procies urls if uses is using webflow url strucutre
-/*
-app.get("/new-homes/region/:slug", async (c) => {
-  const { slug } = c.req.param()
-  //get city and state from slug separated by "-"
-  const [city, state] = slug.split("--")
-  console.log(city, state)
-
-  //const redirectUrl = `/new-homes/${state}/${city}/communities`
-  const url = new URL(`${webflowDomain}/new-homes/${state}/${city}/communities`)
-  const originalUrl = new URL(c.req.url)
-  for (const [key, value] of originalUrl.searchParams) {
-    url.searchParams.set(key, value)
-  }
-  return c.redirect(url.toString())
-})
-*/
 
 //handle rediect to communities page
 app.get("/new-homes/communities/:slug", async (c) => {
